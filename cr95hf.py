a3d955ea2fb09db78e1b88e420bb01988d62d800
adambartyzal <adambartyzal@gmail.com>
2020-10-21 20:47:48 +0200

    it's a class now

---
+++ cr95hf.py
@@ -0,0 +1,83 @@
+import serial as s
+import time as t
+
+class Cr95hf:
+  """Class for controling cr95hf over serial line"""
+
+  def __init__(self):
+    """Constructor of nfc object with default values"""
+    self.ser = s.Serial('/dev/ttyUSB0', 57600,timeout=1)
+    self.CL1 = ""
+    self.CL2 = ""
+
+  def wake(self):
+    self.ser.write(b"\x55")
+    self.ser.read(1)
+
+  def echo(self):
+    self.ser.write(b"\x55")
+    if (self.ser.read(1) != b"\x55"):
+      print('echo error')
+
+  def info(self):
+    self.ser.write(b"\x01\x00")
+    print(f'IDN: {self.ser.read(17).hex()}')
+
+  def protocol(self, type):
+    if (type == 'A'):
+      self.ser.write(b"\x02\x02\x02\x00")
+    elif(type == 'B'):
+      self.ser.write(b"\x02\x02\x03\x01")
+    elif(type == 'N'):
+      self.ser.write(b"\x02\x02\x00\x00")
+    else:
+      print('Wrong Protocol Selection')
+    resp = self.ser.read(2)
+    if (resp.hex() == "0000"):
+      print(f'RF Type {type}')
+    else:
+      print('RF error')
+
+  def reqA(self):
+    """sends REQA and returns length of UID (-1 if none)"""
+    self.ser.write(b"\x04\x02\x26\x07")
+    resp = self.ser.read(7)
+    if (resp.hex() == "8700"):
+      print('No card')
+    elif (resp.hex() == "80050400280000"):
+      return 4
+    elif (resp.hex() == "80054403280000" or resp.hex() == "80054400280000"):
+      return 7
+    else:
+      return -1
+
+  def request(self,type):
+    """sends REQ(type) and returns length of UID (-1 if none)"""
+    if (type == 'A'):
+      self.ser.write(b"\x04\x02\x26\x07")
+    elif(type == 'B'):
+        self.ser.write(b"\x04\x03\x05\x00\x00")
+
+    resp = self.ser.read(14)
+    if (resp.hex() == "8700"):
+      print('No card')
+    elif (resp.hex() == "80050400280000"):
+      return 4
+    elif (resp.hex() == "80054403280000" or resp.hex() == "80054400280000"):
+      return 7
+    else:
+      return -1
+
+  def anticol1(self):
+    """cascade level 1 anticolision"""
+    self.ser.write(b"\x04\x03\x93\x20\x08")
+    self.CL1 = self.ser.read(10).hex().upper()
+
+  def anticol2(self):
+    """cascade level 2 anticolision"""
+    select1 = b'\x04\x08\x93\x70' + bytearray.fromhex(self.CL1[4:16])
+    self.ser.write(select1)
+    resp = self.ser.read(8)
+    self.ser.write(b"\x04\x03\x95\x20\x08")
+    self.CL2 = self.ser.read(10).hex().upper()
+
---
+++ test.py
@@ -0,0 +1,25 @@
+from cr95hf import Cr95hf
+
+UID = ''
+
+nfc = Cr95hf()
+nfc.wake()
+nfc.echo()
+nfc.info()
+nfc.protocol('A')
+uidLength = nfc.request('A')
+
+if (uidLength == 4):
+  nfc.anticol1()
+  UID = nfc.CL1[4:12]
+elif(uidLength == 7):
+  nfc.anticol1()
+  nfc.anticol2()
+  UID = nfc.CL1[6:12] + nfc.CL2[4:12]
+if (len(UID) != 0):
+  print(f'Serial Num: {UID}')
+nfc.protocol('N')
+
+nfc.protocol('B')
+nfc.request('B')
+nfc.protocol('N')
