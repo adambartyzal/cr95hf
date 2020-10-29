07ca47f87e9cd335713e3eaf9644bc9257edc23c
adambartyzal <adambartyzal@gmail.com>
2020-10-29 10:00:46 +0100

    describes reality

--- calcs.py
+++ calcs.py
@@ -10,43 +10,41 @@ def par(*args):
 
 def compute(f):
   Cin = 22e-12 #datasheet
-  Rin = 330 * 2
+  Rin = 80e3 #datasheet
+  Rout = 27 #datasheet
   L0 = 470e-9 #vláďa vybral
   C0 = 220e-12 #vláďa vybral
-  Rout = 27 #datasheet
+  
+  Ratt = 330 #podle demo desky
+  C1 = 70e-12
+  C2 = 180e-12
+
   Zout = Rout
   w = 2 * m.pi * f
-
-  C1 = 47e-12
-  CS = C1 * 2
-  CP = 147e-12
 
   Lant = 600e-9 #eDesign Antenna
   Rant = 0.24 #2 multimetry
   Zant = Rant + 1j * w * Lant
-  Zant = 2.3 + 53 * 1j # změřeno VNA
-  Qant = w * Lant / Rant
+  #Zant = 2.3 + 53 * 1j # změřeno VNA
 
+  ZC0 = 1 / (1j * w * C0 / 2)
+  ZL0 = 1j * w * L0 * 2
 
-  ZC0 = 1 / (1j * w * C0)
-  ZL0 = 1j * w * L0
-
-  ZCS = 1 / (1j * w * CS)
-  ZCP = 1 / (1j * w * CP)
-
+  ZC1 = 1 / (1j * w * C1 * 2)
+  ZC2 = 1 / (1j * w * C2)
 
   ZCin = 1 / (1j * w * Cin)
-  Zin = ZCin + Rin
+  Zin = par(ZCin,Rin) + Ratt * 2
 
-  Zload = ZL0 + par(ZC0,ZCS + par(Zin,Zant,ZCP))
+  Zload = ZL0 + par(ZC0,ZC1 + par(Zin,Zant,ZC2))
 
   fc = 1/(2 * m.pi * m.sqrt(L0 * C0))
 
   return Zload
-  #print(f'fc = {round((fc/1e6),2)} MHz, Zout = {Zout}, Zload = {cmath.polar(Zload)}')
+  #pRattt(f'fc = {round((fc/1e6),2)} MHz, Zout = {Zout}, Zload = {cmath.polar(Zload)}')
 
 
-freqs = range(10000000,20000000,1000)
+freqs = range(int(10e6),int(20e6),int(1e4))
 Zmags = []
 Zangs = []
 for x in freqs:
@@ -59,6 +57,6 @@ ax_right = ax_left.twinx()
 
 ax_left.plot(freqs, Zmags, color='black')
 ax_right.plot(freqs, Zangs, color='red')
-plt.ylim(-m.pi,m.pi)
+plt.ylim(-m.pi/2,m.pi/2)
 plt.grid()
 plt.show()
