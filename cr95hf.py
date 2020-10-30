726e13b9e1af4939cc39400f35888d7201ab7419
adambartyzal <adambartyzal@gmail.com>
2020-10-30 12:02:47 +0100

    wupb and arc_b reading edits

--- cr95hf.py
+++ cr95hf.py
@@ -39,7 +39,7 @@ class Cr95hf:
     if (type == 'A'):
       self.ser.write(b"\x02\x02\x02\x00")
     elif(type == 'B'):
-      self.ser.write(b"\x02\x02\x03\x01")
+      self.ser.write(b"\x02\x02\x03\x01") # B, 106 up, 106 down, CRC append
     elif(type == 'N'):
       self.ser.write(b"\x02\x02\x00\x00")
     else:
@@ -54,13 +54,11 @@ class Cr95hf:
     """Reads ARC_B register value."""
     self.ser.write(b"\x09\x03\x68\x00\x01")
     resp = self.ser.read(2)
-    if (resp.hex() == "0000"):
-      print(f'Gain set')
-    else:
-      print('Error')
-    self.ser.write(b"\x09\x03\x68\x00\x01")
-    resp = self.ser.read(2)
-    print(resp.hex())
+    if (resp.hex() != "0000"):
+      print('ARC_B read error')
+    self.ser.write(b"\x08\x03\x69\x01\x00")
+    resp = self.ser.read(3)
+    print(f'Current settings: {resp.hex()[4:6].upper()}')
 
   def setArcB(self,index,gain):
     """
@@ -68,12 +66,18 @@ class Cr95hf:
       Parameters:
         index (char): 1 = 10%; 2 = 17%; 3 = 25%; 4 = 30%; 5 = 33%; 6 = 36%; D = 95% 
         gain (char): 0 = 34dB; 1 = 32dB; 3 =  27dB; 7 = 20dB; F = 8dB 
+      Defaults:
+        Type A: 'DF'
+        Type B: '2F'
+      Recommended for demoboard:
+        Type A: 'D3'
+        Type B: '20'
     """
-    arc_b = bytearray.fromhex(gain + index)
-    self.ser.write(b"\x09\x04\x68\x01\x01" + arc_b)
+    arc_b = b"\x09\x04\x68\x01\x01" + bytearray.fromhex(index + gain)
+    self.ser.write(arc_b)
     resp = self.ser.read(2)
     if (resp.hex() == "0000"):
-      print(f'Gain set')
+      print(f'Gain {index + gain} set')
     else:
       print('Gain setting error')
 
@@ -105,6 +109,12 @@ class Cr95hf:
       return 7
     else:
       return -1
+
+  def wakeupB(self):
+    """Sends WUPB (same as REQB with wakeup bit up)."""
+    self.ser.write(b"\x04\x03\x05\x00\x08")
+    resp = self.ser.read(14)
+    print(resp.hex())
 
   def anticol1(self):
     """Cascade level 1 anticolision."""
